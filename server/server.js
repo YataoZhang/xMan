1a0a2a261c8afb2a7d6f4e3ff0fb1851c3a4ce92
zhangyatao <xiaodezhang@foxmail.com>
2015-09-25 18:21:13 +0800

    modified server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 var PORT = 3000;
+
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
+var mine = require('./mime').types;
+var path = require('path');
 var formPost = function (content, response, cookie) {
+    "use strict";
     var header = {
         'content-type': 'text/html',
         'set-cookie': cookie
@@ -26,8 +31,34 @@ var server = http.createServer(function (request, response) {
         response.end('this is cors demo');
         return;
     }
-    response.writeHead(500);
-    response.end('path error')
+    var realPath = path.join("../", pathname);
+    var ext = path.extname(realPath);
+    ext = ext ? ext.slice(1) : 'unknown';
+    fs.exists(realPath, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+            response.write("This request URL " + pathname + " was not found on this server.");
+            response.end();
+        } else {
+            fs.readFile(realPath, "binary", function (err, file) {
+                if (err) {
+                    response.writeHead(500, {
+                        'Content-Type': 'text/plain'
+                    });
+                    response.end(err);
+                } else {
+                    var contentType = mine[ext] || "text/plain";
+                    response.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    response.write(file, "binary");
+                    response.end();
+                }
+            });
+        }
+    });
 });
 server.listen(PORT, function () {
     console.log('start over')
